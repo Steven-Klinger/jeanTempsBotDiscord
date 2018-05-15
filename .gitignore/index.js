f76cac49aa457ecec65b80596ccce0c441162c20
Steven <9880918+StevenVorthil@users.noreply.github.com>
2018-05-15 16:29:33 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const botClient = new Discord.Client();
+
+//Client opérationnel 
+botClient.on('ready', function() {
+    botClient.user.setActivity('Attends des réponses').catch(console.error);
+})
+
+botClient.on('message', function(message) {
+    // envoie un message toutes les heures après le $loop
+    if(message.content === '$loop') {
+        var interval = setInterval (function () {
+            message.channel.send('<:hourglass_flowing_sand:445953586560761895> ' +
+            '@everyone vous serez priés de répondre et/ou de prendre votre ticket pour la Gamescom :)' +
+            '<:hourglass:445525485037027340>').catch(console.error)
+        }, 1000 * 60 * 60);
+    }
+})
+
+//Login du bot sur notre serveur
+botClient.login('NDQ1OTM0MDQzODEzNTExMTY4.DdxvTg.pblntB4uQyVmx88UDd3ReWYxjXI');
